48ec0decece748f08cbe8792782cc793ddee484c
Ciro Vargas de Souza Marques Lucena Silva <cirobsb_br@yahoo.com.br>
2024-10-06 21:35:09 -0300

    Modelo de probabilidade

--- cluster_classificacao/app.py
+++ cluster_classificacao/app.py
@@ -5,73 +5,40 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Carregar o modelo treinado e as colunas esperadas
-
-
-# Definir os labels dos clusters
 cluster_labels = {
-    0: 'Cluster 1 - Baixo Risco',
-    1: 'Cluster 2 - Médio Risco',
-    2: 'Cluster 3 - Alto Risco'
+    0: 'Cluster 1 ',
+    1: 'Cluster 2 ',
+    2: 'Cluster 3 '
 }
 
-
-# Função para ajustar as colunas do dataframe de entrada
 def adjust_input_columns(input_data):
-    # Garantir que todas as colunas esperadas estejam presentes
     for col in model_columns:
         if col not in input_data:
-            input_data[col] = 0  # Adicionar coluna faltante com valor 0
+            input_data[col] = 0
 
-    # Garantir a mesma ordem de colunas
     input_data = input_data[model_columns]
     return input_data
 
 
-# Rota para clusterizar o cliente e retornar o label do cluster
 @app.route('/cluster', methods=['POST'])
 def cluster_risk():
     try:
-        # Receber os dados JSON
         data = request.get_json()
 
-        # Converter os dados para dataframe
         input_data = pd.DataFrame([data])
 
-        print('1')
-
-        # Criar dummies para variáveis categóricas
         input_data['risco_inadimplencia'] = get_risco_inadimplencia(data)
         input_data['faixa_etaria'] = get_faixa_etaria(data['idade'])
         input_data['nivel_renda'] = get_nivel_renda(data['renda'])
-        print('2')
-        # input_data = input_data.drop(['etnia', 'genero', 'casa_propria', 'outras_rendas', 'estado_civil', 'escolaridade'], axis=1)
-        print('3')
         input_data = pd.get_dummies(input_data, columns=['risco_inadimplencia', 'faixa_etaria', 'nivel_renda'])
-        print('4')
-        # Ajustar as colunas do input para garantir consistência com o treinamento
         input_data = adjust_input_columns(input_data)
-        print('5')
-        # Fazer a predição do cluster
         cluster_id = model.predict(input_data)[0]
-        print('6')
-        # Obter o label correspondente
         cluster_label = cluster_labels.get(cluster_id, 'Cluster Desconhecido')
-        print('7')
-        print('cluster id')
-        print(cluster_id)
-        print('cluster label')
-        print(cluster_label)
-        # Retornar o ID do cluster e o label
         return jsonify({'cluster_id': int(cluster_id), 'cluster_label': cluster_label})
 
     except Exception as e:
-        print('8', flush=True)
-        print(str(e), flush=True)
         return jsonify({'error': str(e)}), 400
 
-
-# Funções para risco, faixa etária e nível de renda
 def get_risco_inadimplencia(data):
     if data['inadimplente'] == 0 and data['renda'] > 9000:
         return 'Baixo Risco'
--- cluster_classificacao/train_model.py
+++ cluster_classificacao/train_model.py
@@ -4,32 +4,24 @@ from sklearn.cluster import KMeans
 import joblib
 
 
-# Função para treinar o modelo de clusterização com base nas categorias
 def train_cluster_model():
-    # Carregar dataset
     df = pd.read_csv('dataset.csv')
 
-    # Adicionar colunas categóricas para risco, faixa etária e nível de renda
     df['risco_inadimplencia'] = df.apply(get_risco_inadimplencia, axis=1)
     df['faixa_etaria'] = df['idade'].apply(get_faixa_etaria)
     df['nivel_renda'] = df['renda'].apply(get_nivel_renda)
 
     df = df.drop(['etnia', 'genero', 'casa_propria', 'outras_rendas', 'estado_civil', 'escolaridade'], axis=1)
 
-    # Criar dummies para variáveis categóricas
     df = pd.get_dummies(df, columns=['risco_inadimplencia', 'faixa_etaria', 'nivel_renda'])
 
-    # Escolher as colunas relevantes para a clusterização
     X = df.drop(['inadimplente', 'idade', 'renda'], axis=1)
 
-    # Aplicar o KMeans para clusterizar os dados
     kmeans = KMeans(n_clusters=3, random_state=42)
     df['cluster'] = kmeans.fit_predict(X)
 
-    # Salvar o modelo treinado
     joblib.dump(kmeans, 'model_kmeans_risk_cluster.joblib')
 
-    # Salvar as colunas usadas pelo modelo no treinamento
     columns = X.columns.tolist()
     with open('model_columns_cluster.pkl', 'wb') as f:
         joblib.dump(columns, f)
@@ -45,7 +37,6 @@ def get_risco_inadimplencia(row):
         return 'Médio Risco'
 
 
-# Definir categorias de faixa etária
 def get_faixa_etaria(idade):
     if 18 <= idade <= 25:
         return 'Jovens Adultos'
@@ -57,7 +48,6 @@ def get_faixa_etaria(idade):
         return 'Sêniores'
 
 
-# Definir categorias de nível de renda
 def get_nivel_renda(renda):
     if renda < 3000:
         return 'Baixa Renda'
